2dc36411b560aa111edc03df9570cfaa46482713
Manuel Ramos <manuelramos175@gmail.com>
2020-10-25 12:57:35 -0300

    refactor test setup

--- test_extractor.py
+++ test_extractor.py
@@ -2,21 +2,17 @@ import unittest
 from extractor import extract_data
 
 class TestExtractor(unittest.TestCase):
+
+    def setUp(self):
+        self.str_obj = '{\"guid\": \"1234\", \"content\": {\"type\": \"text/html\"}}'
+
     def test_when_keys_are_passed_with_up_to_one_level_deep(self):
-        str_obj = """
-            {
-                "guid": "1234",
-                "content": {
-                    "type": "text/html"
-                },
-            }
-        """
         keys = ['guid', 'content.type']
         expected_result = {'guid': '1234', 'content.type': 'text/html'}
 
-        actual_result = extract_data(str_obj, keys)
+        actual_result = extract_data(self.str_obj, keys)
 
-        self.assertEquals(actual_result,
-                          expected_result,
-                          'It should return a dictionary with the keys expressed in dot notation and its values')
+        self.assertDictEqual(actual_result,
+                             expected_result,
+                             'It should return a dictionary with the keys expressed in dot notation and its values')
 
