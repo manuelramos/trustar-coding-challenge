41bdd15e03fc7b477733f12bfca0a71a8509e13f
Manuel Ramos <manuelramos175@gmail.com>
2020-10-25 12:25:20 -0300

    refactor test name, fn name and clean code

--- extractor.py
+++ extractor.py
@@ -1,13 +1,39 @@
 import json
 from functools import reduce
 
-def extract(str_obj, keys):
+def extract_data(str_obj, keys):
+    """
+    Extract data from a string-like json object.
+    Returns a dictionary made up of the 'keys' passed as arguments (expressed in dot notation)
+    and the corresponding value.
+
+    Example
+    =======
+    INPUT:
+        a= '{
+                "guid": "1234",
+                "content": {
+                    "type": "text/html",
+                    "title": "Challenge 1",
+                    "entities": [ "1.2.3.4", "wannacry", "malware.com"]
+                },
+                "score": 74,
+                "time": 1574879179
+        }'
+        b = ["guid", "content.entities", "score", "score.sign"]
+
+        > extract_data(a,b)
+
+    OUTPUT:
+        { "guid": "1234", "content.entities": [ "1.2.3.4", "wannacry", "malware.com"], "score": 74 }
+
+    """
     obj = json.loads(str_obj)
     result = {}
     for key in keys:
-        result[key] = _get_value(obj, key)
+        result[key] = get_value(obj, key)
     return result
 
 
-def _get_value(obj, key):
+def get_value(obj, key):
     return reduce(lambda a,b: a[b], key.split('.'), obj)
--- test_extractor.py
+++ test_extractor.py
@@ -1,38 +1,22 @@
 import unittest
-from extractor import extract
+from extractor import extract_data
 
-class TestExtractorMethods(unittest.TestCase):
-    """
-    Example:
-    '{
-        "guid": "1234",
-        "content": {
-            "type": "text/html",
-            "title": "Challenge 1",
-            "entities": [ "1.2.3.4", "wannacry", "malware.com"]
-        },
-        "score": 74,
-        "time": 1574879179
-    }'
-    """
-    def test_when_pass_single_prop_should_return_single_value(self):
-        # obj = '{\"guid\":\"1234\",\"content\":{\"type\":\"text\/html\",\"title\":\"Challenge 1\",\"entities\":[\"1.2.3.4\",\"wannacry\",\"malware.com\"]},\"score\":74,\"time\":1574879179}'
+class TestExtractor(unittest.TestCase):
+    def test_when_keys_are_passed_with_up_to_one_level_deep(self):
         str_obj = """
             {
                 "guid": "1234",
                 "content": {
-                    "type": "text/html",
-                    "title": "Challenge 1",
-                    "entities": [ "1.2.3.4", "wannacry", "malware.com"]
+                    "type": "text/html"
                 },
-                "score": 74,
-                "time": 1574879179
             }
         """
         keys = ['guid', 'content.type']
-
         expected_result = {'guid': '1234', 'content.type': 'text/html'}
-        actual_result = extract(str_obj, keys)
 
-        self.assertEquals(actual_result, expected_result, 'should return a dict with the corresponding values')
+        actual_result = extract_data(str_obj, keys)
+
+        self.assertEquals(actual_result,
+                          expected_result,
+                          'It should return a dictionary with the keys expressed in dot notation and its values')
 
